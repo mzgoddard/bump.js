5452fd40903ed713bddbaab6384c2d927a8754c4
Michael "Z" Goddard <mzgoddard@gmail.com>
2012-08-21 23:29:11 -0400

    update SphereBoxCollisionAlgorithm to bullet repo r2546
    r2546 has a simpler implementation of sphere box but if the port to JS
    is accurate c++ implementation is still buggy.
    
    http://code.google.com/p/bullet/source/browse/trunk/src/BulletCollision/CollisionDispatch/btSphereBoxCollisionAlgorithm.cpp?r=2546

--- src/BulletCollision/CollisionDispatch/SphereBoxCollisionAlgorithm.js
+++ src/BulletCollision/CollisionDispatch/SphereBoxCollisionAlgorithm.js
@@ -89,44 +89,72 @@
         this._super();
       },
 
-      processCollision: function( body0, body1, dispatchInfo, resultOut ) {
+      processCollision: function( body0Wrap, body1Wrap, dispatchInfo, resultOut ) {
         if ( this.manifoldPtr === null ) {
           return;
         }
 
-        var sphereObj = this.swapped ? body1 : body0,
-            boxObj = this.swapped ? body0 : body1,
-            sphere0 = sphereObj.getCollisionShape(),
-            box1 = boxObj.getCollisionShape();
+        var sphereObjWrap = this.swapped? body1Wrap : body0Wrap;
+        var boxObjWrap = this.swapped? body0Wrap : body1Wrap;
+
+        var pOnBox = getVector3();
 
         var normalOnSurfaceB = getVector3();
-        var pOnBox = getVector3(), pOnSphere = getVector3();
-        var sphereCenter = sphereObj.getWorldTransform().getOrigin();
+        var penetrationDepthRef = { value: 0 };
+        var sphereCenter = sphereObjWrap.getWorldTransform().getOrigin();
+        var sphere0 = sphereObjWrap.getCollisionShape();
         var radius = sphere0.getRadius();
-  
-        var dist = this.getSphereDistance( boxObj, pOnBox, pOnSphere, sphereCenter, radius );
+        var maxContactDistance = this.manifoldPtr.getContactBreakingThreshold();
 
         resultOut.setPersistentManifold( this.manifoldPtr );
 
-        if ( dist < Bump.SIMD_EPSILON )
+        if ( this.getSphereDistance( boxObjWrap, pOnBox, normalOnSurfaceB, penetrationDepthRef, sphereCenter, radius, maxContactDistance ) )
         {
-          pOnBox.subtract( pOnSphere, normalOnSurfaceB ).normalize();
-
           /// report a contact. internally this will be kept persistent, and contact reduction is done
-
-          resultOut.addContactPoint( normalOnSurfaceB, pOnBox, dist );
-
+          resultOut.addContactPoint( normalOnSurfaceB, pOnBox, penetrationDepthRef.value );
         }
 
         if ( this.ownManifold )
         {
-          // if ( this.manifoldPtr.getNumContacts() )
+          // if (m_manifoldPtr->getNumContacts())
           // {
             resultOut.refreshContactPoints();
           // }
         }
 
-        delVector3( normalOnSurfaceB, pOnBox, pOnSphere );
+        // var sphereObj = this.swapped ? body1 : body0,
+        //     boxObj = this.swapped ? body0 : body1,
+        //     sphere0 = sphereObj.getCollisionShape(),
+        //     box1 = boxObj.getCollisionShape();
+        // 
+        // var normalOnSurfaceB = getVector3();
+        // var pOnBox = getVector3(), pOnSphere = getVector3();
+        // var sphereCenter = sphereObj.getWorldTransform().getOrigin();
+        // var radius = sphere0.getRadius();
+        //   
+        // var dist = this.getSphereDistance( boxObj, pOnBox, pOnSphere, sphereCenter, radius );
+        // 
+        // resultOut.setPersistentManifold( this.manifoldPtr );
+        // 
+        // if ( dist < Bump.SIMD_EPSILON )
+        // {
+        //   pOnBox.subtract( pOnSphere, normalOnSurfaceB ).normalize();
+        // 
+        //   /// report a contact. internally this will be kept persistent, and contact reduction is done
+        // 
+        //   resultOut.addContactPoint( normalOnSurfaceB, pOnBox, dist );
+        // 
+        // }
+        // 
+        // if ( this.ownManifold )
+        // {
+        //   // if ( this.manifoldPtr.getNumContacts() )
+        //   // {
+        //     resultOut.refreshContactPoints();
+        //   // }
+        // }
+
+        // delVector3( normalOnSurfaceB, pOnBox, pOnSphere );
 
         // // `refreshContactPoints` is only necessary when using persistent
         // // contact points. Otherwise all points are newly added.
@@ -140,187 +168,118 @@
         return 1;
       },
 
-      getSphereDistance: function( boxObj, pointOnBox, v3PointOnSphere, sphereCenter, fRadius ) {
+      getSphereDistance: function( boxObjWrap, pointOnBox, normal, penetrationDepthRef, sphereCenter, fRadius, maxContactDistance ) {
 
-        var margins;
-        var bounds = [ null, null ];
-        var boxShape = boxObj.getCollisionShape();
+        var boxShape = boxObjWrap.getCollisionShape();
+        var boxHalfExtent = boxShape.getHalfExtentsWithoutMargin();
+        var boxMargin = boxShape.getMargin();
+        penetrationDepthRef.value = 1.0;
 
-        bounds[ 1 ] = boxShape.getHalfExtentsWithoutMargin().clone( getVector3() );
-        bounds[ 0 ] = bounds[ 1 ].negate( getVector3() );
+        // convert the sphere position to the box's local space
+        var m44T = boxObjWrap.getWorldTransform();
+        var sphereRelPos = getVector3();
+        m44T.invXform( sphereCenter, sphereRelPos );
 
-        margins = boxShape.getMargin(); //also add sphereShape margin?
+        // Determine the closest point to the sphere center in the box
+        var closestPoint = sphereRelPos.clone( getVector3() );
+        closestPoint.x = ( Math.min( boxHalfExtent.x, closestPoint.x ) );
+        closestPoint.x = ( Math.max( -boxHalfExtent.x, closestPoint.x ) );
+        closestPoint.y = ( Math.min( boxHalfExtent.y, closestPoint.y ) );
+        closestPoint.y = ( Math.max( -boxHalfExtent.y, closestPoint.y ) );
+        closestPoint.z = ( Math.min( boxHalfExtent.z, closestPoint.z ) );
+        closestPoint.z = ( Math.max( -boxHalfExtent.z, closestPoint.z ) );
+        
+        var intersectionDist = fRadius + boxMargin;
+        var contactDist = intersectionDist + maxContactDistance;
+        normal = sphereRelPos.subtract( closestPoint, normal );
 
-        var m44T = boxObj.getWorldTransform();
-
-        var boundsVec = [ null, null ];
-        var fPenetration;
-
-        boundsVec[ 0 ] = bounds[ 0 ].clone( getVector3() );
-        boundsVec[ 1 ] = bounds[ 1 ].clone( getVector3() );
-
-        var marginsVec = getVector3().setValue( margins, margins, margins );
-
-        // add margins
-        bounds[ 0 ].addSelf( marginsVec );
-        bounds[ 1 ].subtractSelf( marginsVec );
-
-        /////////////////////////////////////////////////
-
-        // btVector3
-        var tmp = getVector3(),
-            prel = getVector3(),
-            n = GSDn,
-            normal = getVector3(),
-            v3P = getVector3();
-        var fSep = 10000000.0, fSepThis; // btScalar
-
-        // convert  point in local space
-        prel = m44T.invXform( sphereCenter, prel );
-
-        var bFound = false;
-
-        v3P.assign( prel );
-
-        for (var i = 0; i < 6; i++ ) {
-          var j = i < 3 ? 0 : 1;
-          if ( ( fSepThis = ( ( v3P.subtract( bounds[ j ], tmp ).dot( n[ i ] ) ) ) ) > 0.0 ) {
-            v3P.subtractSelf( n[ i ].multiplyScalar( fSepThis, tmp ) );
-            bFound = true;
-          }
+        //if there is no penetration, we are done
+        var dist2 = normal.length2();
+        if ( dist2 > contactDist * contactDist ) {
+          return false;
         }
 
-        //
+        var distance;
 
-        if ( bFound ) {
-          bounds[ 0 ].assign( boundsVec[ 0 ] );
-          bounds[ 1 ].assign( boundsVec[ 1 ] );
-
-          // normal = ( prel - v3P ).normalize();
-          prel.subtract( v3P, normal ).normalize();
-          // pointOnBox = v3P + normal * margins;
-          v3P.add( normal.multiplyScalar( margins, tmp ), pointOnBox );
-          // v3PointOnSphere = prel - normal * fRadius;
-          prel.subtract( normal.multiplyScalar( fRadius, tmp ), v3PointOnSphere );
-
-          if ( ( ( v3PointOnSphere.subtract( pointOnBox, tmp ) ).dot( normal ) ) > 0.0 ) {
-
-            delVector3(
-              bounds[ 0 ], bounds[ 1 ], boundsVec[ 0 ], boundsVec[ 1 ],
-              tmp, normal, prel, v3P
-            );
-
-            return 1.0;
-          }
-
-          // transform back in world space
-          m44T.transform( pointOnBox, tmp );
-          pointOnBox.assign( tmp );
-          m44T.transform( v3PointOnSphere, tmp );
-          v3PointOnSphere.assign( tmp );
-          var fSeps2 = ( pointOnBox.subtract( v3PointOnSphere, tmp ) ).length2();
-
-          //if this fails, fallback into deeper penetration case, below
-          if ( fSeps2 > Bump.SIMD_EPSILON ) {
-            fSep = - Math.sqrt( fSeps2 );
-            normal = ( pointOnBox.subtract( v3PointOnSphere, normal ) );
-            normal.multiplyScalarSelf( 1.0 / fSep );
-          }
-
-          delVector3(
-            bounds[ 0 ], bounds[ 1 ], boundsVec[ 0 ], boundsVec[ 1 ],
-            tmp, normal, prel, v3P
-          );
-
-          return fSep;
+        //special case if the sphere center is inside the box
+        if ( dist2 === 0.0 ) {
+          distance = -this.getSpherePenetration( boxHalfExtent, sphereRelPos, closestPoint, normal );
+        }
+        else //compute the penetration details
+        {
+          distance = normal.length();
+          normal.divideScalarSelf( distance );
         }
 
-        //////////////////////////////////////////////////
-        // Deep penetration case
-
-        fPenetration = this.getSpherePenetration(
-          boxObj, pointOnBox, v3PointOnSphere, sphereCenter,
-          fRadius, bounds[0], bounds[1] );
-
-        bounds[ 0 ].assign( boundsVec[0] );
-        bounds[ 1 ].assign( boundsVec[1] );
-
-        delVector3(
-          bounds[ 0 ], bounds[ 1 ], boundsVec[ 0 ], boundsVec[ 1 ],
-          tmp, normal, prel, v3P
-        );
-
-        if ( fPenetration <= 0.0 ) {
-          return ( fPenetration - margins );
-        } else {
-          return 1.0;
-        }
-      },
-
-      getSpherePenetration: function( boxObj, pointOnBox, v3PointOnSphere, sphereCenter, fRadius, aabbMin, aabbMax ) {
-
-        var bounds = [ null, null ];
-
-        bounds[ 0 ] = aabbMin;
-        bounds[ 1 ] = aabbMax;
-
-        var p0 = getVector3(),
-            tmp = getVector3(),
-            prel = getVector3(),
-            n = GSDn,
-            normal = getVector3();
-        var fSep = -10000000.0, fSepThis;
-
-        // set p0 and normal to a default value to shup up GCC
-        // p0.setValue( 0.0, 0.0, 0.0 );
-        // normal.setValue( 0.0, 0.0, 0.0 );
-
-        var m44T = boxObj.getWorldTransform();
-
-        // convert  point in local space
-        m44T.invXform( sphereCenter, prel );
-
-        ///////////
-
-        for ( var i = 0; i < 6; i++ ) {
-          var j = i < 3 ? 0 : 1;
-          if ( ( fSepThis = ( ( prel.subtract( bounds[ j ], tmp ) ).dot( n[ i ] ) ) - fRadius ) > 0.0 ) {
-
-            delVector3(
-              tmp, normal, prel, p0
-            );
-
-            return 1.0;
-          }
-          if ( fSepThis > fSep ) {
-            p0.assign( bounds[ j ] );
-            normal.assign( n[ i ] );
-            fSep = fSepThis;
-          }
-        }
-
-        prel.subtract( normal.
-          multiplyScalar(
-            normal.dot( ( prel.subtract( p0, tmp ) ) ),
-            tmp
-          ), pointOnBox );
-        pointOnBox.add(
-          normal.multiplyScalar( fSep, tmp ),
-          v3PointOnSphere );
+        pointOnBox = closestPoint.add( normal.multiplyScalar( boxMargin, pointOnBox ), pointOnBox );
+        //      v3PointOnSphere = sphereRelPos - (normal * fRadius);    
+        penetrationDepthRef.value = distance - intersectionDist;
 
         // transform back in world space
-        tmp = m44T.transform( pointOnBox, tmp );    
+        var tmp = m44T.transform( pointOnBox, getVector3() );
         pointOnBox.assign( tmp );
-        tmp  = m44T.transform( v3PointOnSphere, tmp );
-        v3PointOnSphere.assign( tmp );
-        // normal = ( pointOnBox.subtract( v3PointOnSphere, normal ) ).normalize();
+        //      tmp = m44T(v3PointOnSphere);
+        //      v3PointOnSphere = tmp;
+        tmp = m44T.getBasis().multiplyVector( normal, tmp );
+        normal.assign( tmp );
 
-        delVector3(
-          tmp, normal, prel, p0
-        );
+        return true;
 
-        return fSep;
+      },
 
+      getSpherePenetration: function( boxHalfExtent, sphereRelPos, closestPoint, normal ) {
+        //project the center of the sphere on the closest face of the box
+        var faceDist = boxHalfExtent.x - sphereRelPos.x;
+        var minDist = faceDist;
+        closestPoint.x = ( boxHalfExtent.x );
+        normal.setValue( 1.0, 0.0, 0.0 );
+
+        faceDist = boxHalfExtent.x + sphereRelPos.x;
+        if ( faceDist < minDist )
+        {
+                minDist = faceDist;
+                closestPoint.assign( sphereRelPos );
+                closestPoint.x = ( -boxHalfExtent.x );
+                normal.setValue( -1.0, 0.0, 0.0 );
+        }
+
+        faceDist = boxHalfExtent.y - sphereRelPos.y;
+        if ( faceDist < minDist )
+        {
+                minDist = faceDist;
+                closestPoint.assign( sphereRelPos );
+                closestPoint.y = ( boxHalfExtent.y );
+                normal.setValue( 0.0, 1.0, 0.0 );
+        }
+
+        faceDist = boxHalfExtent.y + sphereRelPos.y;
+        if ( faceDist < minDist )
+        {
+                minDist = faceDist;
+                closestPoint.assign( sphereRelPos );
+                closestPoint.y = ( -boxHalfExtent.y );
+                normal.setValue( 0.0, -1.0, 0.0 );
+        }
+
+        faceDist = boxHalfExtent.z - sphereRelPos.z;
+        if ( faceDist < minDist )
+        {
+                minDist = faceDist;
+                closestPoint.assign( sphereRelPos );
+                closestPoint.z = ( boxHalfExtent.z );
+                normal.setValue( 0.0, 0.0, 1.0 );
+        }
+
+        faceDist = boxHalfExtent.z + sphereRelPos.z;
+        if ( faceDist < minDist )
+        {
+                minDist = faceDist;
+                closestPoint.assign( sphereRelPos );
+                closestPoint.z = ( -boxHalfExtent.z );
+                normal.setValue( 0.0, 0.0, -1.0 );
+        }
+
+        return minDist;
       },
 
       getAllContactManifolds: function( manifoldArray ) {
--- tests/unit/BulletCollision/CollisionDispatch/test.SphereBoxCollisionAlgorithm.js
+++ tests/unit/BulletCollision/CollisionDispatch/test.SphereBoxCollisionAlgorithm.js
@@ -7,9 +7,9 @@ function truncate( v, precision ) {
   return v;
 }
 
-test( 'sphere-box x axis', function() {
+test( 'sphere-box X axis', function() {
 
-  expect( 10 );
+  expect( 16 );
 
   var manifold = Bump.PersistentManifold.create();
   var caci = Bump.CollisionAlgorithmConstructionInfo.create();
@@ -45,39 +45,147 @@ test( 'sphere-box x axis', function() {
 
   ok( sbca, 'SphereBoxCollisionAlgorithm created.' );
 
-  var pOnBox = Bump.Vector3.create(),
+  var normal = Bump.Vector3.create(),
+      pOnBox = Bump.Vector3.create(),
       pOnSphere = Bump.Vector3.create(),
       sphereCenter = Bump.Vector3.create( 0, 0, 0 ),
+      penetrationDepthRef = { value: 0 },
       dist;
 
-  console.log( sphereBody.getWorldTransform().getOrigin() );
-  // console.log( sbca.getSphereDistance( boxBody, pOnBox, pOnSphere, sphereCenter, sphereShape.getRadius() ), pOnBox, pOnSphere );
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
 
-  dist = sbca.getSphereDistance( boxBody, pOnBox, pOnSphere, sphereCenter, sphereShape.getRadius() );
-
-  ok( dist < Bump.SIMD_EPSILON, 'sphereDistance less than EPSILON.' );
+  ok( dist, 'sphereDistance returns true.' );
   deepEqual( sphereCenter, Bump.Vector3.create( 0, 0, 0 ), 'sphereCenter unchanged.' );
   deepEqual( boxShape.getHalfExtentsWithMargin(), Bump.Vector3.create( 0.5, 0.5, 0.5 ) );
   deepEqual( boxBody.getWorldTransform().getOrigin(), Bump.Vector3.create( 0.9, 0, 0 ) );
-  deepEqual( pOnSphere, Bump.Vector3.create( 0.5, 0, 0 ), 'point on sphere is correct.' );
-  deepEqual( pOnBox, Bump.Vector3.create( 0.9 - boxShape.getHalfExtentsWithoutMargin().x, 0, 0 ), 'point on box is correct.' );
+  // deepEqual( pOnSphere, Bump.Vector3.create( 0.5, 0, 0 ), 'point on sphere is correct.' );
+  deepEqual( normal, Bump.Vector3.create( -1, 0, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -0.1 );
+  deepEqual( pOnBox, Bump.Vector3.create( 0.9 - boxShape.getHalfExtentsWithMargin().x, 0, 0 ), 'point on box is correct.' );
 
   boxBody.getWorldTransform().setOrigin( Bump.Vector3.create( 0.1, 0, 0 ) );
-  dist = sbca.getSphereDistance( boxBody, pOnBox, pOnSphere, sphereCenter, sphereShape.getRadius() );
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
 
-  ok( dist < Bump.SIMD_EPSILON, 'sphereDistance less than EPSILON. (' + dist + ')' );
-  deepEqual( pOnSphere, Bump.Vector3.create( 0.5, 0, 0 ), 'point on sphere is correct.' );
-
+  ok( dist, 'sphereDistance returns true' );
+  deepEqual( normal, Bump.Vector3.create( -1, 0, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -0.9 );
   deepEqual(
     truncate( pOnBox, 1000 ),
     Bump.Vector3.create(
       0.1 -
-        boxShape.getHalfExtentsWithoutMargin().x +
-        boxShape.getMargin(),
+        boxShape.getHalfExtentsWithMargin().x,
       0,
       0 ),
     'point on box is correct.' );
 
-  console.log( dist, pOnBox, pOnSphere );
+  boxBody.getWorldTransform().setOrigin( Bump.Vector3.create( -0.1, 0, 0 ) );
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
+
+  ok( dist, 'sphereDistance returns true' );
+  deepEqual( normal, Bump.Vector3.create( 1, 0, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -0.9 );
+  deepEqual(
+    truncate( pOnBox, 1000 ),
+    Bump.Vector3.create(
+      -0.1 +
+        boxShape.getHalfExtentsWithMargin().x,
+      0,
+      0 ),
+    'point on box is correct.' );
+
+});
+
+
+test( 'sphere-box Y axis', function() {
+
+  expect( 16 );
+
+  var manifold = Bump.PersistentManifold.create();
+  var caci = Bump.CollisionAlgorithmConstructionInfo.create();
+
+  var sphereShape = Bump.SphereShape.create( 0.5 );
+  var boxShape = Bump.BoxShape.create( Bump.Vector3.create( 20, 20, 20 ) );
+
+  var localInertia = Bump.Vector3.create();
+  sphereShape.calculateLocalInertia( 1, localInertia );
+
+  var startTransform = Bump.Transform.create();
+  startTransform.setIdentity();
+  startTransform.setOrigin( Bump.Vector3.create( 0, 0, 0 ) );
+
+  var myMotionState = Bump.DefaultMotionState.create( startTransform );
+  var rbInfo = Bump.RigidBody.RigidBodyConstructionInfo.create(
+    1, myMotionState, sphereShape, localInertia );
+
+  var sphereBody = Bump.RigidBody.create( rbInfo );
+
+  startTransform = Bump.Transform.create();
+  startTransform.setIdentity();
+  startTransform.setOrigin( Bump.Vector3.create( 0, 0.9, 0 ) );
+
+  myMotionState = Bump.DefaultMotionState.create( startTransform );
+  rbInfo = Bump.RigidBody.RigidBodyConstructionInfo.create(
+    1, myMotionState, boxShape, localInertia );
+
+  var boxBody = Bump.RigidBody.create( rbInfo );
+
+  var sbca = Bump.SphereBoxCollisionAlgorithm.create(
+    manifold, caci, sphereBody, boxBody );
+
+  ok( sbca, 'SphereBoxCollisionAlgorithm created.' );
+
+  var normal = Bump.Vector3.create(),
+      pOnBox = Bump.Vector3.create(),
+      pOnSphere = Bump.Vector3.create(),
+      sphereCenter = Bump.Vector3.create( 0, 0, 0 ),
+      penetrationDepthRef = { value: 0 },
+      dist;
+
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
+
+  ok( dist, 'sphereDistance returns true.' );
+  deepEqual( sphereCenter, Bump.Vector3.create( 0, 0, 0 ), 'sphereCenter unchanged.' );
+  deepEqual( boxShape.getHalfExtentsWithMargin(), Bump.Vector3.create( 20, 20, 20 ) );
+  deepEqual( boxBody.getWorldTransform().getOrigin(), Bump.Vector3.create( 0, 0.9, 0 ) );
+  // deepEqual( pOnSphere, Bump.Vector3.create( 0.5, 0, 0 ), 'point on sphere is correct.' );
+  deepEqual( normal, Bump.Vector3.create( 0, -1, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -0.6 );
+  deepEqual( pOnBox, Bump.Vector3.create( 0, 0.9 - boxShape.getHalfExtentsWithMargin().y, 0 ), 'point on box is correct.' );
+
+  boxBody.getWorldTransform().setOrigin( Bump.Vector3.create( 0, 0.1, 0 ) );
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
+
+  ok( dist, 'sphereDistance returns true' );
+  deepEqual( normal, Bump.Vector3.create( 0, -1, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -1.4 );
+  deepEqual(
+    truncate( pOnBox, 1000 ),
+    Bump.Vector3.create(
+      0,
+      0.1 -
+        boxShape.getHalfExtentsWithMargin().y,
+      0 ),
+    'point on box is correct.' );
+
+  boxBody.getWorldTransform().setOrigin( Bump.Vector3.create( 0, -0.1, 0 ) );
+  dist = sbca.getSphereDistance(
+    boxBody, pOnBox, normal, penetrationDepthRef, sphereCenter, sphereShape.getRadius(), 10 );
+
+  ok( dist, 'sphereDistance returns true' );
+  deepEqual( normal, Bump.Vector3.create( 0, 1, 0 ) );
+  equal( Math.ceil( penetrationDepthRef.value * 1000 ) / 1000, -1.4 );
+  deepEqual(
+    truncate( pOnBox, 1000 ),
+    Bump.Vector3.create(
+      0,
+      -0.1 +
+        boxShape.getHalfExtentsWithMargin().y,
+      0 ),
+    'point on box is correct.' );
 
 });
